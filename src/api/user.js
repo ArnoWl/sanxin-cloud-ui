7909d9b1c3b7cde20918778192d00ef367e3cfcf
arno <bleachpjj@163.com>
2019-08-26 09:55:24 +0800

    Sanxin UI

---
+++ src/api/user.js
@@ -0,0 +1,25 @@
+import request from '@/utils/request'
+import { getToken } from '@/utils/auth'
+
+export function getInfo(token) {
+  return request({
+    url: '/user/getInfo',
+    method: 'post',
+    params: {"token":getToken()}
+  })
+}
+
+export function getMenus(roleid){
+  return request({
+    url: '/user/getMenus',
+    method: 'post',
+    params: {"token":getToken(),"roleid":roleid}
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/loginOut',
+    method: 'post'
+  })
+}
