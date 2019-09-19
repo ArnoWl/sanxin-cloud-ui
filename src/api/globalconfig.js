66564fcf2d8e49bf94bc8810995d58caac230b01
sky-xiao <15574814866@163.com>
2019-09-19 09:20:52 +0800

    Sanxin UI

--- src/api/globalconfig.js
+++ src/api/globalconfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-  baseURL : 'http://127.0.0.1:8001/'
+  // baseURL : 'http://47.244.167.166:8001/'
+  baseURL: 'http://127.0.0.1:8001/'
 }
--- src/main.js
+++ src/main.js
@@ -20,9 +20,8 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
-
 import globalconfig from './api/globalconfig.js'
-Vue.prototype.Global=globalconfig
+Vue.prototype.Global = globalconfig
 
 /**
  * If you don't want to use mock-server
@@ -32,10 +31,10 @@ Vue.prototype.Global=globalconfig
  * Currently MockJs will be used in the production environment,
  * please remove it before going online! ! !
  */
-import { mockXHR } from '../mock'
-if (process.env.NODE_ENV === 'production') {
-  mockXHR()
-}
+// import { mockXHR } from '../mock'
+// if (process.env.NODE_ENV === 'production') {
+//   mockXHR()
+// }
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
